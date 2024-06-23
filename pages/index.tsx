58014d64af401f57053aa7be59fb325b72982afd
Krym <gumimash@gmail.com>
2024-06-23 23:48:49 +0200

    home cmp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "@/components/layout/Home";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-sky-600">Hello</h1>
-    </div>
+    <>
+      <Header label="Home" />
+    </>
   );
 }
