1a17d5dde2b29a0a8a43fd20e71e3203540116cc
Krym <gumimash@gmail.com>
2024-06-24 01:51:10 +0200

    modal wip

---
+++ components/Modal.tsx
@@ -0,0 +1,25 @@
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+}) => {
+  return <div></div>;
+};
+
+export default Modal;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
+import Modal from "@/components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Modal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
